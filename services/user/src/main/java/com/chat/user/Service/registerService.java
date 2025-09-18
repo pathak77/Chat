ad759dd7acd2f997fb15e155de311130d26252cc
Aayush Pathak <aayushpathak77@gmail.com>
2025-09-18 22:19:26 +0545

    ?'

---
+++ services/user/src/main/java/com/chat/user/Service/registerService.java
@@ -0,0 +1,8 @@
+class Register{
+@Autowired 
+	userRepo userUserRepo      userRepo;
+private registerUserd[Cprivate ResponseEntity<String> registerUser(RegisterDto reg){
+return ResponseEntity.....y.status(HttpStatus.Good_Reque[CGOOD_REQUEST).body("request accepted");
+}
+}
+i:wq
